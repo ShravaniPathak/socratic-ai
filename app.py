49718aeed57939fd0a347500b90b6e132ab0c4b7
Shravani Pathak <140315107+ShravaniPathak@users.noreply.github.com>
2024-10-02 23:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,6 @@ def get_response():
     response = generate_socratic_response(user_input)
     return jsonify({'response': response})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
